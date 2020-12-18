2c6d7b017a132d70075ffb95bf7c3669dc4be029
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2020-12-18 17:45:01 -0300

    fix: Deploy settings

--- src/repositories/subjectsRepo.js
+++ src/repositories/subjectsRepo.js
@@ -6,7 +6,7 @@ async function getSubjectIdByItsName(name) {
         const result = await db.query('SELECT id FROM subjects WHERE name = $1', [name]);
         return result.rows[0].id;
     } catch (e) {
-        return e;
+        return null;
     }
 }
 
